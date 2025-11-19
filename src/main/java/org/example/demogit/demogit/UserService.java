7cf484e08c6f95c07ee086434fddc9571c4d7d7a
Avelinka29 <avelina58100@gmail.com>
2025-11-20 00:34:44 +0300

    test commit-6

--- src/main/java/org/example/demogit/demogit/UserService.java
+++ src/main/java/org/example/demogit/demogit/UserService.java
@@ -4,6 +4,6 @@ public class UserService {
     public void helloworld(){
         System.out.println("hello world");
         System.out.println("     ");
-
+        System.out.println("3765637");
     }
 }
